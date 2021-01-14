f60dc6848ede0c8994fe24f1719d73d89da7fcf4
abdallah.sarayrah <abdallah.dev85@gmail.com>
2021-01-14 19:09:01 +0200

    local ip for testing

--- lib/static/staticVars.dart
+++ lib/static/staticVars.dart
@@ -3,13 +3,13 @@
 class StaticVars {
   //I've made the API URL variable as static because I want to access it
   // globally and I don't want to change it every time in the home file.
-  // static var url = "http://$localIp/apps/myapps/barcodescan/apis/";
-  static var url = webHostUrl;
+  static var url = "http://$localIp/apps/myapps/barcodescan/apis/";
+  // static var url = webHostUrl;
   static var webHostUrl = "https://barcodescaner.000webhostapp.com/APIs/";
 
   //This is the IP of the remote server that I'll use it when I want to test the
   // app on the remote server or to publish it.
-  static var localIp = "192.168.0.29";
+  static var localIp = "192.168.1.9";
   static var serverIp = "193.188.88.148";
 
   static String userId = "0";
